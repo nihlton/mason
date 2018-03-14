dfb778aa44716d9e8bcbc7f00c042a90e5cad2d5
greg archer <nihlton@gmail.com>
2018-03-14 08:29:10 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Mason from './components/Mason'
 import './App.css';
-
 class App extends Component {
-	
-	constructor (props) {
-		super(props)
-		
-		let hipsum = `Lorem ipsum dolor amet banjo helvetica bitters forage. Gluten-free shaman listicle put a bird on it, craft beer PBR&B neutra meditation health goth typewriter XOXO. Portland chillwave offal intelligentsia artisan pok pok 3 wolf moon VHS shabby chic tousled church-key ethical craft beer. Craft beer raw denim tumblr celiac viral pop-up tbh single-origin coffee chillwave etsy tousled. Stumptown man braid biodiesel blog poke. Thundercats williamsburg narwhal, portland DIY +1 selfies yr. Prism subway tile put a bird on it, 3 wolf moon knausgaard XOXO tousled fashion axe. Actually skateboard edison bulb marfa hoodie vice fanny pack waistcoat selfies church-key enamel pin snackwave locavore. Activated charcoal sartorial intelligentsia man bun`
-		this.state = {
-			items: (new Array(30)).fill(0).map(i => hipsum.substr(0, 50 + (Math.random() * (hipsum.length - 51)) ))
-		}
-	}
-	
-	
+  constructor(props) {
+    super(props)
+    let hipsum = `Lorem ipsum dolor amet banjo helvetica bitters forage. Gluten-free shaman listicle put a bird on it, craft beer PBR&B neutra meditation health goth typewriter XOXO. Portland chillwave offal intelligentsia artisan pok pok 3 wolf moon VHS shabby chic tousled church-key ethical craft beer. Craft beer raw denim tumblr celiac viral pop-up tbh single-origin coffee chillwave etsy tousled. Stumptown man braid biodiesel blog poke. Thundercats williamsburg narwhal, portland DIY +1 selfies yr. Prism subway tile put a bird on it, 3 wolf moon knausgaard XOXO tousled fashion axe. Actually skateboard edison bulb marfa hoodie vice fanny pack waistcoat selfies church-key enamel pin snackwave locavore. Activated charcoal sartorial intelligentsia man bun`
+    this.state = {
+      items: (new Array(30)).fill(0).map(i => hipsum.substr(0, 50 + (Math.random() * (hipsum.length - 51))))
+    }
+  }
 
-	render() {
-		const responsiveItems = [<h1>responsive</h1>, ...this.state.items]
-		const static3Items = [<h1>static 3 columns</h1>, ...this.state.items]
-
-		return (
-			<div className='App'>
-                <Mason columns={{large: 4, medium: 2, small: 1}}>
-                    {responsiveItems.map((item, index) => <div key={index} className='my-item' ><div>{item}</div></div>)}
-                </Mason>
-				<hr />
-				<Mason columns={{default: '3'}}>
-					{static3Items.map((item, index) => <div key={index} className='my-item' ><div>{item}</div></div>)}
-				</Mason>
-			</div>
-		)
-	}
+  render() {
+    const responsiveItems = [<h1>responsive</h1>, ...this.state.items]
+    const static3Items = [<h1>static 3 columns</h1>, ...this.state.items]
+    return (
+      <div className='App'>
+        <Mason columns={{large: 4, medium: 2, small: 1}}>
+          {responsiveItems.map((item, index) => <div key={index} className='my-item'>
+            <div>{item}</div>
+          </div>)}
+        </Mason>
+        <hr />
+        <Mason columns={{default: '3'}}>
+          {static3Items.map((item, index) => <div key={index} className='my-item'>
+            <div>{item}</div>
+          </div>)}
+        </Mason>
+      </div>
+    )
+  }
 }
-
 export default App;
--- src/components/Mason.js
+++ src/components/Mason.js
@@ -1,49 +1,47 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ResizeObserver from 'resize-observer-polyfill'
-import { debounce } from 'lodash'
-
+import {debounce} from 'lodash'
 import './Mason.css'
-
 export default class Mason extends Component {
-	componentDidMount () {
-		new ResizeObserver(() => {this.positionChildren()}).observe(this.refs.container)
-        this.positionChildren()
-	}
-	
-	positionChildren = () => {
-		let container = this.refs.container
-        let breakPoint = window.getComputedStyle(container, ':before').getPropertyValue('content').replace(/\"/g, '')
-		let columns = +this.props.columns[breakPoint] || +this.props.columns.default || 1
-		let columnDebt = new Array(columns).fill(0)
-		let children = [].slice.call(this.refs.container.children)
 
-		children.forEach((child, index) => {
-			let column = index % columns
-			let rowChildren = children.slice(index - column, index - column + columns)
-			let maxHeight = Math.max( ...rowChildren.map(child => child.offsetHeight))
-			let debt = maxHeight - child.offsetHeight
-			
-			child.style.transform = index > columns - 1 ? `translateY(${-columnDebt[column]}px)` : ''
-			columnDebt[column] = columnDebt[column] + debt
-		})
+  componentDidMount() {
+    new ResizeObserver(() => {
+      this.positionChildren()
+    }).observe(this.refs.container)
+    this.positionChildren()
+  }
 
-		window.setTimeout(() => {
-            let lastChildren = children.slice(Math.max(children.length - columns, 1))
-            let childBottomEdge = lastChildren.map(child => {
-            	let childRect = child.getBoundingClientRect()
-            	return ((childRect.top + childRect.height))
-            })
-            let containerHeight =  (Math.max(...childBottomEdge)  - container.getBoundingClientRect().top) + 'px'
+  positionChildren = () => {
+    const container = this.refs.container
+    const breakPoint = window.getComputedStyle(container, ':before').getPropertyValue('content').replace(/\"/g, '')
+    const columns = +this.props.columns[breakPoint] || +this.props.columns.default || 1
+    const columnDebt = new Array(columns).fill(0)
+    const children = [].slice.call(this.refs.container.children)
 
-            container.style.height = containerHeight
-		}, 0)
-	}
-	
-	render () {
-		let masonClasses = Object.keys(this.props.columns).map(breakPoint => `mason-columns-${breakPoint}-${this.props.columns[breakPoint]}`)
+    children.forEach((child, index) => {
+      const column = index % columns
+      const rowChildren = children.slice(index - column, index - column + columns)
+      const maxHeight = Math.max(...rowChildren.map(child => child.offsetHeight))
+      const debt = maxHeight - child.offsetHeight
+      child.style.transform = index > columns - 1 ? `translateY(${-columnDebt[column]}px)` : ''
+      columnDebt[column] = columnDebt[column] + debt
+    })
 
-		return <div className={['mason-container', ...masonClasses].join(' ')} ref='container'>
-			{this.props.children}
-		</div>
-	}
+    window.setTimeout(() => {
+      const lastChildren = children.slice(Math.max(children.length - columns, 1))
+      const childBottomEdge = lastChildren.map(child => {
+        const childRect = child.getBoundingClientRect()
+        return ((childRect.top + childRect.height))
+      })
+      container.style.height = (Math.max(...childBottomEdge) - container.getBoundingClientRect().top) + 'px'
+    }, 0)
+  }
+
+  render() {
+    const columns = this.props.columns
+    const masonClasses = Object.keys(this.props.columns).map(breakPoint => `mason-columns-${breakPoint}-${columns[breakPoint]}`)
+    return <div className={['mason-container', ...masonClasses].join(' ')} ref='container'>
+      {this.props.children}
+    </div>
+  }
 }
