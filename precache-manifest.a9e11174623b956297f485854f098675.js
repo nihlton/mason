df2c4fdc9070946fb265c1a85bec75ae46531661
greg archer <nihlton@gmail.com>
2020-04-12 21:03:36 -0700

    set homepage in package JSON

---
+++ precache-manifest.a9e11174623b956297f485854f098675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b808f21e942236ad19395bba455541",
+    "url": "/mason/index.html"
+  },
+  {
+    "revision": "8707ac3c8ba02abe4659",
+    "url": "/mason/static/css/main.1cdbeeda.chunk.css"
+  },
+  {
+    "revision": "578fba1eddb1ca9615e4",
+    "url": "/mason/static/js/2.d3f17b63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mason/static/js/2.d3f17b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8707ac3c8ba02abe4659",
+    "url": "/mason/static/js/main.baa9ab9e.chunk.js"
+  },
+  {
+    "revision": "8f9a924ebf1998064c28",
+    "url": "/mason/static/js/runtime-main.85dc7525.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58ed83298967b4e57def251829a8e8f3.js"
+  "/mason/precache-manifest.a9e11174623b956297f485854f098675.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mason/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
